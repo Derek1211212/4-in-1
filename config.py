f642f91b801f556ca2e5f3fbd35d8b1fd13f6f0a
Derek1211212 <derickbill3@gmail.com>
2024-12-03 23:18:23 -0800

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,11 @@ import os
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'fa470fe714e44404511cbad16224f52777068d05bb5c29bc')  # Fallback to default if not set
-    MYSQL_HOST = os.getenv('MYSQL_HOST', 'localhost')  # Default to localhost
-    MYSQL_USER = os.getenv('MYSQL_USER', 'root')
-    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'root')
+    MYSQL_HOST = os.getenv('MYSQL_HOST', '108.181.157.253')  # Default to localhost
+    MYSQL_USER = os.getenv('MYSQL_USER', 'admin')
+    MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'KlZW5SSG')
     MYSQL_DB = os.getenv('MYSQL_DB', 'sys')
-    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 3306))  # Convert port to an integer
+    MYSQL_PORT = int(os.getenv('MYSQL_PORT', 10009))  # Convert port to an integer
 
 
 MAIL_SERVER = 'smtp.gmail.com'
